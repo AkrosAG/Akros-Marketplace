e2709b485ed14fd35e19a353e43d37deac4ccc0b
Richter Jan <richja@akros.ch>
2022-01-26 14:36:59 +0100

    renamed and refactored names of services and folder names

--- infrastructure/restservices/clean_startup.sh
+++ infrastructure/restservices/clean_startup.sh
@@ -3,5 +3,5 @@ rm -rf ../pg_data_am
 rm -rf ../pg_data_userdb
 docker container prune -f
 docker image prune -f
-docker image rm restservices_data-service-backend:latest restservices_administration-business-ui:latest
+docker image rm restservices_marketplace-service:latest restservices_admin-ui:latest
 docker-compose up
