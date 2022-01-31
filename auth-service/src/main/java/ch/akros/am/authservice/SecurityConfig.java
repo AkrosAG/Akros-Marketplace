2af2873e77c19f5316a55b86626f0b6262b0c440
betalla <berthold.talla@akros.ch>
2022-01-31 21:38:47 +0100

    added config oauth-service redirecting http to https

---
+++ auth-service/src/main/java/ch/akros/am/authservice/SecurityConfig.java
@@ -0,0 +1,17 @@
+package ch.akros.am.authservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http.requiresChannel(channel -> channel.anyRequest().requiresSecure())
+               .authorizeRequests(authorize -> authorize.anyRequest().permitAll())
+               .build();
+  }
+}
