95f8a9d5c88b1de352306e16772e9af4a4579ede
Stefan Le-Minh <stefanleminh@gmail.com>
2022-02-01 15:29:27 +0100

    Finish Webcomponent PoC

--- webcomponents/search-webcomponent/src/app/app.module.ts
+++ webcomponents/search-webcomponent/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+import {Injector, NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { CategoriesListComponent } from './search/categories-list/categories-list.component';
-import { SearchFormComponent } from './search/search-form/search-form.component';
-import { SearchComponent } from './search/search.component';
-import { DynamicFormFieldComponent } from './shared/form/dynamic-form-field.component';
-import { SwitchCasesDirective } from './shared/form/switch-cases.directive';
+import {AppComponent} from './app.component';
+import {CategoriesListComponent} from './search/categories-list/categories-list.component';
+import {SearchFormComponent} from './search/search-form/search-form.component';
+import {SearchComponent} from './search/search.component';
+import {DynamicFormFieldComponent} from './shared/form/dynamic-form-field.component';
+import {SwitchCasesDirective} from './shared/form/switch-cases.directive';
 import {StoreModule} from '@ngrx/store';
 import {EffectsModule} from '@ngrx/effects';
 import {MarketPlaceEffects} from './data/store/marketplace.effects';
@@ -15,13 +15,14 @@ import {marketplaceStoreName} from './data/store/marketplace.selector';
 import {marketplaceReducer} from './data/store/marketplace.reducer';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { CategoriesService } from './data/services/categories.service';
-import { FormFieldControlService } from './shared/form/form-field-control.service';
-import { FormFieldsBuilderService } from './utils/formFieldsBuilderService';
-import { RestHelperService } from './utils/restHelperService';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {CategoriesService} from './data/services/categories.service';
+import {FormFieldControlService} from './shared/form/form-field-control.service';
+import {FormFieldsBuilderService} from './utils/formFieldsBuilderService';
+import {RestHelperService} from './utils/restHelperService';
 import {LoggerModule, NgxLoggerLevel} from 'ngx-logger';
-import { environment } from 'src/environments/environment';
+import {environment} from 'src/environments/environment';
+import {createCustomElement} from '@angular/elements';
 
 @NgModule({
   declarations: [
@@ -61,9 +62,17 @@ import { environment } from 'src/environments/environment';
     FormFieldControlService,
     FormFieldsBuilderService,
   ],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent, SearchComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const customElement = createCustomElement(AppComponent, {
+      injector: this.injector,
+    });
+    customElements.define('search-component', customElement);
+  }
+  ngDoBootStrap() {}
+}
 
 export function httpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http);
