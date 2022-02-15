56861a3dea14f89c21ab3cb4279aab64fd75782c
Stefan Le-Minh <stefanleminh@gmail.com>
2022-02-15 17:18:34 +0100

    Ignore Cypress-Tests in Jest

--- am-ui/jest.config.js
+++ am-ui/jest.config.js
@@ -1,6 +1,7 @@
 // jest.config.js (javascript file)
 module.exports = {
   preset: 'jest-preset-angular',
+  testPathIgnorePatterns: ['<rootDir>/cypress/'],
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   coverageThreshold: {
     global: {
