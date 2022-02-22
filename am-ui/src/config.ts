574192fe8c48157147cbbdf3ecb4642449d17030
CristianAyerbe <cristian.ayerbe@akros.ch>
2022-02-22 18:21:25 +0100

    lint fix

--- am-ui/src/config.ts
+++ am-ui/src/config.ts
@@ -1,4 +1,16 @@
 export const appConfig: any = {
   appLanguage: 'de',
-  selectCounterValues: ['1', '1.5', '2', '2.5', '3', '3.5', '4', '4.5', '5', '5.5', '6']
+  selectCounterValues: [
+    '1',
+    '1.5',
+    '2',
+    '2.5',
+    '3',
+    '3.5',
+    '4',
+    '4.5',
+    '5',
+    '5.5',
+    '6',
+  ],
 };
