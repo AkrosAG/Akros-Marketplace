8f2aa44800e48d557f4d7358f0930d479a88a0a7
Stefan Le-Minh <stefanleminh@gmail.com>
2022-03-03 09:50:43 +0100

    Adjust build scripts for prod

--- am-ui/webcomponents/build_scripts/build-webcomponents.js
+++ am-ui/webcomponents/build_scripts/build-webcomponents.js
@@ -1,27 +1,37 @@
-const fs = require("fs");
-const resolve = require("path").resolve;
-const join = require("path").join;
-const cp = require("child_process");
-const os = require("os");
+const fs = require('fs');
+const resolve = require('path').resolve;
+const join = require('path').join;
+const cp = require('child_process');
+const os = require('os');
 
 // get library path
-const path = resolve(__dirname, "../../webcomponents/");
+const path = resolve(__dirname, '../../webcomponents/');
 
 fs.readdirSync(path).forEach(function (mod) {
   const modPath = join(path, mod);
 
   // ensure path has package.json
-  if (!fs.existsSync(join(modPath, "package.json"))) {
+  if (!fs.existsSync(join(modPath, 'package.json'))) {
     return;
   }
 
   // npm binary based on OS
-  const npmCmd = os.platform().startsWith("win") ? "npm.cmd" : "npm";
+  const npmCmd = os.platform().startsWith('win') ? 'npm.cmd' : 'npm';
+  const isProduction = process.argv[2] === '--prod';
 
-  // install folder
-  cp.spawn(npmCmd, ["run", "build"], {
-    env: process.env,
-    cwd: modPath,
-    stdio: "inherit",
-  });
+  if (isProduction) {
+    console.log('Building for production!');
+    cp.spawn(npmCmd, ['run', 'build:prod'], {
+      env: process.env,
+      cwd: modPath,
+      stdio: 'inherit',
+    });
+  } else {
+    console.log('Building for dev!');
+    cp.spawn(npmCmd, ['run', 'build'], {
+      env: process.env,
+      cwd: modPath,
+      stdio: 'inherit',
+    });
+  }
 });
