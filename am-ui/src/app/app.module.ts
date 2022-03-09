34928e08baef77609d3a1a789c192477e89e4ac2
Iliyan Kotsev <iliyan.kotsev@akros.ch>
2022-03-09 15:33:51 +0200

    styles test

--- am-ui/src/app/app.module.ts
+++ am-ui/src/app/app.module.ts
@@ -36,6 +36,7 @@ import {RestHelperService} from './utils/restHelperService';
 import {UserService} from './data/services/login/user.service';
 
 import {environment} from 'src/environments/environment';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 export function httpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http);
@@ -64,7 +65,7 @@ export function MsalInterceptorConfigFactory(): MsalInterceptorConfiguration {
 }
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavbarComponent],
+  declarations: [AppComponent, HomeComponent, NavbarComponent, FooterComponent],
   imports: [
     BrowserModule,
     FormsModule,
