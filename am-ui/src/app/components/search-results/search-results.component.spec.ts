67194f1908aa94dc13b258b86edf1c7609d5e12a
Stefan Le-Minh <stefanleminh@gmail.com>
2022-03-28 16:55:12 +0200

    Fix test for search-result view

--- am-ui/src/app/components/search-results/search-results.component.spec.ts
+++ am-ui/src/app/components/search-results/search-results.component.spec.ts
@@ -1,4 +1,9 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {
+  TranslateFakeLoader,
+  TranslateLoader,
+  TranslateModule,
+} from '@ngx-translate/core';
 
 import {SearchResultsComponent} from './search-results.component';
 
@@ -9,6 +14,14 @@ describe('SearchResultsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SearchResultsComponent],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader,
+          },
+        }),
+      ],
     }).compileComponents();
   });
 
