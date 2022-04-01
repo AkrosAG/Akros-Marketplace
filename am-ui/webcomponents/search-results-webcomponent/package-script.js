6a7efaef57e9bb13244d954fcc186309afdfe333
Stefan Le-Minh <stefanleminh@gmail.com>
2022-04-01 14:59:35 +0200

    Add build:prod command

--- am-ui/webcomponents/search-results-webcomponent/package-script.js
+++ am-ui/webcomponents/search-results-webcomponent/package-script.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra');
 const concat = require('concat');
 
 build = async () => {
-  const files = ['./dist/App.jsx'];
+  const files = ['./dist/App.js'];
   await fs.ensureDir('../../src/assets/scripts/webcomponents');
   await concat(
     files,
