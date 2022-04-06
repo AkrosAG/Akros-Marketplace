f2f332a44931b49e1a0feae4fed760a0200d1b58
CristianAyerbe <cristian.ayerbe@akros.ch>
2022-04-06 17:29:21 +0200

    translations files for create vue web component

---
+++ am-ui/webcomponents/create-ad-webcomponent/src/locales/i18n.js
@@ -0,0 +1,92 @@
+import {createI18n} from 'vue-i18n';
+
+export const i18n = createI18n({
+  locale: 'en',
+  legacy: false,
+  messages: {
+    en: {
+      offer: 'I offer',
+      request: 'Im looking for',
+      categories: {
+        accomodation: {
+          title: 'Accomodation',
+          types: {
+            room: 'Room',
+            apartment: 'Apartment',
+            house: 'House',
+            parking: 'Parking space',
+          },
+          type: 'Type of accomodation',
+          region: 'Region',
+          postalCode: 'Zipcode',
+          address: 'Address, No.',
+          rooms: 'Rooms',
+          size: 'Living space (m2)',
+          price: 'Rent (CHF)',
+          floor: 'Floor',
+          priceUnit: 'Price unit',
+          priceUnits: {
+            month: 'Per month',
+            week: 'Per Week',
+            day: 'Per Day',
+          },
+          furnished: 'Furnished',
+          description: 'Description',
+          expectations: 'My expectation on the tenant',
+          about: 'Brief description about me',
+          phone: 'Phone number',
+          email: 'E-mail',
+          publish: 'Preview and publish',
+        },
+      },
+      carShare: {
+        title: 'Car sharing',
+        types: {
+          share: 'Car sharing',
+        },
+      },
+    },
+    de: {
+      offer: 'Ich biete',
+      request: 'Ich suche nach',
+      categories: {
+        accomodation: {
+          title: 'Unterkünfte',
+          types: {
+            room: 'Zimmer',
+            apartment: 'Wohnung',
+            house: 'Haus',
+            parking: 'Parkplatz',
+          },
+          type: 'Art der Unterkunft',
+          region: 'Region',
+          postalCode: 'PLZ',
+          address: 'Strasse, Nr.',
+          rooms: 'Zimmer',
+          size: 'Wohnfläche (m2)',
+          price: 'Miete (CHF)',
+          floor: 'Etage',
+          priceUnit: 'Preiseinheit',
+          priceUnits: {
+            month: 'Pro Monat',
+            week: 'Pro Woche',
+            day: 'Pro Tag',
+          },
+          furnished: 'Möbliert',
+          description: 'Beschreibung',
+          expectations: 'Meine Erwartung an den Mieter',
+          about: 'Kurze Beschreibung über mich',
+          phone: 'Telefonnummer',
+          email: 'E-mail',
+          publish: 'Vorschau anzeigen und veröffentlichen',
+        },
+      },
+      carShare: {
+        title: 'Car sharing',
+        types: {
+          share: 'Car sharing',
+        },
+      },
+    },
+  },
+});
