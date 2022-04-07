4b27e44a1b0858fa196cded9fc01ce68abd1e264
Stefan Le-Minh <stefanleminh@gmail.com>
2022-04-07 17:16:49 +0200

    Adjust reference in clean startup script

--- infrastructure/start_all_local/clean_startup.sh
+++ infrastructure/start_all_local/clean_startup.sh
@@ -3,5 +3,5 @@ rm -rf ../pg_data_am
 rm -rf ../pg_data_userdb
 docker container prune -f
 docker image prune -f
-docker image rm restservices_marketplace-service:latest restservices_admin-ui:latest restservices_auth-service:latest
+docker image rm start_all_local_marketplace-service:latest start_all_local_admin-ui:latest start_all_local_auth-service:latest
 docker-compose up
