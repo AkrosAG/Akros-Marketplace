6feabd14ad06210cffdc38f8fe60af2d20a851aa
Stefan Le-Minh <stefanleminh@gmail.com>
2022-04-11 12:36:28 +0200

    Adjust reverse proxy

--- infrastructure/start_all_local/clean_startup.sh
+++ infrastructure/start_all_local/clean_startup.sh
@@ -3,5 +3,5 @@ rm -rf ../pg_data_am
 rm -rf ../pg_data_userdb
 docker container prune -f
 docker image prune -f
-docker image rm start_all_local_marketplace-service:latest start_all_local_admin-ui:latest start_all_local_auth-service:latest
+docker image rm start_all_local_marketplace-service:latest start_all_local_admin-ui:latest start_all_local_auth-service:latest start_all_local_client-ui:latest
 docker-compose up
