16a26a25ace31ed5d98909bc18e014a427dd9dc1
Stefan Le-Minh <stefanleminh@gmail.com>
2022-04-19 17:19:43 +0200

    Revert changes on create-ad

--- am-ui/webcomponents/create-ad-webcomponent/src/components/CreateAdFields.vue
+++ am-ui/webcomponents/create-ad-webcomponent/src/components/CreateAdFields.vue
@@ -6,8 +6,8 @@
       class="form-field full"
       v-if="
         field.field_type_definition_id === 1 ||
-          field.field_type_definition_id === 2 ||
-          field.field_type_definition_id === 3
+        field.field_type_definition_id === 2 ||
+        field.field_type_definition_id === 3
       "
       v-bind:class="{
         half: field.field_type_definition_id === 2,
@@ -45,8 +45,8 @@
       class="form-field full"
       v-if="
         field.field_type_definition_id === 5 ||
-          field.field_type_definition_id === 6 ||
-          field.field_type_definition_id === 7
+        field.field_type_definition_id === 6 ||
+        field.field_type_definition_id === 7
       "
       v-bind:class="{
         half: field.field_type_definition_id === 6,
@@ -65,7 +65,10 @@
         <option disabled value="">
           {{ t(`categories.${selectedCategory}.${field.key}.title`) }}
         </option>
-        <option v-for="option in field.field_options" v-bind:value="option.key">
+        <option
+          v-for="option in field.field_options"
+          v-bind:value="option.field_option_id"
+        >
           {{
             t(
               `categories.${selectedCategory}.${field.key}.options.${option.key}`
@@ -80,14 +83,14 @@
       class="form-field checkbox half"
       v-if="
         field.field_type_definition_id === 8 ||
-          field.field_type_definition_id === 16
+        field.field_type_definition_id === 16
       "
     >
       <input
         v-bind:id="'create-add-field-' + field.field_id"
         type="checkbox"
         v-bind:placeholder="t(`categories.${selectedCategory}.${field.key}`)"
-        v-model="fieldValues[field.field_id]"
+        v-model="fieldValues[field.key]"
       />
       <label>{{ t(`categories.${selectedCategory}.${field.key}`) }}</label>
     </div>
@@ -137,7 +140,7 @@
       class="form-field half"
       v-if="
         field.field_type_definition_id === 12 ||
-          field.field_type_definition_id === 13
+        field.field_type_definition_id === 13
       "
       v-bind:class="{
         third: field.field_type_definition_id === 13,
@@ -160,7 +163,7 @@
       class="form-field half"
       v-if="
         field.field_type_definition_id === 14 ||
-          field.field_type_definition_id === 15
+        field.field_type_definition_id === 15
       "
       v-bind:class="{
         third: field.field_type_definition_id === 15,
@@ -196,26 +199,26 @@
 </template>
 
 <script setup>
-import {onMounted, ref} from 'vue'
-import {useI18n} from './useI18n'
-import i18n from '../locales/i18n'
+import {onMounted, ref} from 'vue';
+import {useI18n} from './useI18n';
+import i18n from '../locales/i18n';
 
-const props = defineProps({fieldsToShow: Array, selectedCategory: String})
-const emit = defineEmits(['submit'])
-const fieldValues = ref([])
-const fieldKeys = ref([])
-const errors = ref([])
-const counterOptions = ref([1, 2, 3, 4, 5, 6, 7, 8])
-const {t} = useI18n(i18n.global.messages.value)
-const formHasErrors = ref([])
+const props = defineProps({fieldsToShow: Array, selectedCategory: String});
+const emit = defineEmits(['submit']);
+const fieldValues = ref([]);
+const fieldKeys = ref([]);
+const errors = ref([]);
+const counterOptions = ref([1, 2, 3, 4, 5, 6, 7, 8]);
+const {t} = useI18n(i18n.global.messages.value);
+const formHasErrors = ref([]);
 
-function checkField (fieldId) {
+function checkField(fieldId) {
   const emailPatternRegex = new RegExp(
     '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'
-  )
-  const zipCodePatternRegex = new RegExp('[0-9]{4}')
-  const numberPatternRegex = new RegExp('^[0-9]*$')
-  const alphabeticPatternRegex = new RegExp('^((?![0-9]).)*$$')
+  );
+  const zipCodePatternRegex = new RegExp('[0-9]{4}');
+  const numberPatternRegex = new RegExp('^[0-9]*$');
+  const alphabeticPatternRegex = new RegExp('^((?![0-9]).)*$$');
 
   // Static specific validations based on AM categories (currently only accomodation), TODO improve
   switch (this.fieldKeys[fieldId]) {
@@ -226,11 +229,11 @@ function checkField (fieldId) {
         this.fieldValues[fieldId].length > 50 ||
         this.fieldValues[fieldId].length < 1
       ) {
-        this.errors[fieldId] = true
+        this.errors[fieldId] = true;
       } else {
-        this.errors[fieldId] = false
+        this.errors[fieldId] = false;
       }
-      break
+      break;
     // Region: max length 50, min length 1 chars and not numbers
     case 'region':
       if (
@@ -238,11 +241,11 @@ function checkField (fieldId) {
         this.fieldValues[fieldId].length < 1 ||
         !alphabeticPatternRegex.test(this.fieldValues[fieldId])
       ) {
-        this.errors[fieldId] = true
+        this.errors[fieldId] = true;
       } else {
-        this.errors[fieldId] = false
+        this.errors[fieldId] = false;
       }
-      break
+      break;
     // Description, about and expectations: max length 1000, min length 1 chars
     case 'expectations':
     case 'description':
@@ -251,97 +254,97 @@ function checkField (fieldId) {
         this.fieldValues[fieldId].length > 1000 ||
         this.fieldValues[fieldId].length < 1
       ) {
-        this.errors[fieldId] = true
+        this.errors[fieldId] = true;
       } else {
-        this.errors[fieldId] = false
+        this.errors[fieldId] = false;
       }
-      break
+      break;
     // Email: Email format regex
     case 'email':
       if (!emailPatternRegex.test(this.fieldValues[fieldId])) {
-        this.errors[fieldId] = true
+        this.errors[fieldId] = true;
       } else {
-        this.errors[fieldId] = false
+        this.errors[fieldId] = false;
       }
-      break
+      break;
     // Selectos: Ok if not empty
     case 'rooms':
     case 'type':
       if (this.fieldValues[fieldId] !== null) {
-        this.errors[fieldId] = false
+        this.errors[fieldId] = false;
       } else {
-        this.errors[fieldId] = true
+        this.errors[fieldId] = true;
       }
-      break
+      break;
     // Phone number: Number only regex
     case 'phone':
       if (!numberPatternRegex.test(this.fieldValues[fieldId])) {
-        this.errors[fieldId] = true
+        this.errors[fieldId] = true;
       } else {
-        this.errors[fieldId] = false
+        this.errors[fieldId] = false;
       }
-      break
+      break;
     // Zipcode: Four digit only regex
     case 'postalCode':
       if (!zipCodePatternRegex.test(this.fieldValues[fieldId])) {
-        this.errors[fieldId] = true
+        this.errors[fieldId] = true;
       } else {
-        this.errors[fieldId] = false
+        this.errors[fieldId] = false;
       }
-      break
+      break;
     // Date: Selected date not prior to current date
     case 'date':
-      const today = new Date()
-      const selectedDate = new Date(this.fieldValues[fieldId])
+      const today = new Date();
+      const selectedDate = new Date(this.fieldValues[fieldId]);
       if (today.getTime() < selectedDate.getTime()) {
-        this.errors[fieldId] = false
+        this.errors[fieldId] = false;
       } else {
-        this.errors[fieldId] = true
+        this.errors[fieldId] = true;
       }
-      break
+      break;
   }
-  this.formHasErrors = false
+  this.formHasErrors = false;
   this.errors.forEach(err => {
     if (err) {
-      this.formHasErrors = true
+      this.formHasErrors = true;
     }
-  })
+  });
 }
 
-function submit () {
-  const fieldsVals = Object.values(fieldValues.value)
-  const keys = Object.keys(fieldValues.value)
+function submit() {
+  const fieldsVals = Object.values(fieldValues.value);
+  const keys = Object.keys(fieldValues.value);
 
-  let containsErrors = false
+  let containsErrors = false;
 
   fieldValues.value.forEach((fieldValue, i) => {
     // Temp exception for field price_unit as it is at this point not developed
     if (i !== 7) {
       if (!fieldValue) {
-        errors.value[i] = true
-        containsErrors = true
+        errors.value[i] = true;
+        containsErrors = true;
       } else if (fieldValue.toString().length < 1) {
-        errors.value[i] = true
-        containsErrors = true
+        errors.value[i] = true;
+        containsErrors = true;
       }
     }
-  })
+  });
   if (containsErrors) {
     const fields = keys.map((id, i) => {
-      return {field_type_id: id, value: fieldsVals[i]}
-    })
-    emit('submit', fields)
+      return {field_type_id: id, value: fieldsVals[i]};
+    });
+    emit('submit', fields);
   } else {
-    formHasErrors.value = true
+    formHasErrors.value = true;
   }
 }
 
 onMounted(() => {
-  formHasErrors.value = false
+  formHasErrors.value = false;
   props.fieldsToShow.forEach(field => {
-    fieldValues.value[field.field_id] = ''
-    fieldKeys.value[field.field_id] = field.key
-    errors.value[field.field_id] = false
-  })
-})
+    fieldValues.value[field.field_id] = '';
+    fieldKeys.value[field.field_id] = field.key;
+    errors.value[field.field_id] = false;
+  });
+});
 </script>
