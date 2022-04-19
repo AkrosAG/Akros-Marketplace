d137364e87132d789a76d09b50ae7fc62d33285a
Stefan Le-Minh <stefanleminh@gmail.com>
2022-04-19 12:06:03 +0200

    Adjust styles and remove unused styles and methods

--- am-ui/webcomponents/search-results-webcomponent/src/App.jsx
+++ am-ui/webcomponents/search-results-webcomponent/src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css';
 import SearchResultList from './SearchResultList';
 import ReactDOM from 'react-dom';
 import './i18n';
-import IndexStyles from './index.css';
-import AppStyles from './App.css';
+import IndexStyles from './styles/index.css';
+import AppStyles from './styles/App.css';
 import ComponentStyles from './styles/styles.css';
 import React from 'react';
+import ResetStyles from './styles/reset.css';
 
 const parentHandleClick = (topic) => {
   const event = new CustomEvent('openDetailsEvent', {
@@ -48,7 +48,7 @@ class SearchResultsWebComponent extends HTMLElement {
     this.mountPoint = document.createElement('div');
     // Adding custum style sheets for webcomponents to habe them
     const style = document.createElement('style');
-    style.textContent = AppStyles + IndexStyles + ComponentStyles;
+    style.textContent = ResetStyles + AppStyles + IndexStyles + ComponentStyles;
     root.appendChild(style);
     root.appendChild(this.mountPoint);
   }
