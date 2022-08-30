eef8b2bb9bae886e12af0ea2cca30ece9f44a41c
inacfi <filipe.inacio@akros.ch>
2022-08-30 14:31:32 +0200

    remove whitespace from RouterTestingModule in search-results.component.spec.ts

--- am-ui/src/app/components/search-results/search-results.component.spec.ts
+++ am-ui/src/app/components/search-results/search-results.component.spec.ts
@@ -1,7 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {Router} from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 import {
   TranslateFakeLoader,
   TranslateLoader,
