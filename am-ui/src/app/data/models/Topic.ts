4250c0c62ff30f1ab3bbfe58a7d03ac2fa4c5e6e
Ernst Leisi <ernst.leisi@akros.ch>
2022-09-02 10:45:09 +0200

    Add model for topic

---
+++ am-ui/src/app/data/models/Topic.ts
@@ -0,0 +1,9 @@
+import {TopicValue} from './TopicValue'
+
+export class Topic {
+    topic_id: number;
+    category_id: number;
+    subcategory_id: number;
+    request_or_offer: string;
+    topic_values: TopicValue[];
+}
