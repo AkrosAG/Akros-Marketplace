5344be92c4287b8294da261e535f6b0e0cbccd9d
inacfi <filipe.inacio@akros.ch>
2022-09-09 13:04:21 +0200

    fixed lint

--- am-ui/src/app/components/search-result-details/search-result-details-map/search-result-details-map.component.ts
+++ am-ui/src/app/components/search-result-details/search-result-details-map/search-result-details-map.component.ts
@@ -42,9 +42,9 @@ export class SearchResultDetailsMapComponent implements AfterViewInit {
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 100,
       minZoom: 3,
-      attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    });
+      /*eslint-disable-next-line*/
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      });
 
     tiles.addTo(this.map);
     const marker = L.marker([+this.lat, +this.lon]);
