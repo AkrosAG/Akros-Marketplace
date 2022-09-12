0a641004ff1029785500009dd648898972d46c3c
aschiakros <99389985+aschiakros@users.noreply.github.com>
2022-09-12 15:49:39 +0200

    Map test (#164)
    * Map example
    
    * Centering markers and popup with address details
    
    * Add semicolons
    
    * Replace br tag by divs
    
    Co-authored-by: Yordan Yordanov <yordyo@akros.ch>
    Co-authored-by: Ernst Leisi <ernst.leisi@akros.ch>

--- am-ui/webcomponents/search-results-webcomponent/src/App.jsx
+++ am-ui/webcomponents/search-results-webcomponent/src/App.jsx
@@ -6,6 +6,10 @@ import AppStyles from './styles/App.css';
 import ComponentStyles from './styles/styles.css';
 import React from 'react';
 import ResetStyles from './styles/reset.css';
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
+import icon from 'leaflet/dist/images/marker-icon.png';
+import L from 'leaflet';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 const parentHandleClick = (topic) => {
   const event = new CustomEvent('openDetailsEvent', {
@@ -24,6 +28,7 @@ class SearchResultsWebComponent extends HTMLElement {
     super();
     this._results = [];
     this._language = '';
+    this.boundaries = [];
   }
 
   set results(results) {
@@ -46,13 +51,67 @@ class SearchResultsWebComponent extends HTMLElement {
     const root = this.attachShadow({ mode: 'closed' });
     // Create a mount element
     this.mountPoint = document.createElement('div');
-    // Adding custum style sheets for webcomponents to habe them
+    // Adding custom style sheets for webcomponents to habe them
     const style = document.createElement('style');
     style.textContent = ResetStyles + AppStyles + IndexStyles + ComponentStyles;
     root.appendChild(style);
     root.appendChild(this.mountPoint);
   }
+
+  createMarkerFromTopic(topic) {
+    let latValue = topic.topic_values.find(v => v.key === 'lat')?.value;
+    let lonValue = topic.topic_values.find(v => v.key === 'lon')?.value;
+    let addressValue = topic.topic_values.find(v => v.key === 'address')?.value;
+    let postalCodeValue = topic.topic_values.find(v => v.key === 'postalCode')?.value;
+    let regionValue = topic.topic_values.find(v => v.key === 'region')?.value;
+
+    // In case of garbage address input we do not create a marker
+    if (latValue === "" || lonValue === "" || (latValue == 0 && lonValue == 0)) {
+      return;
+    }
+    return (
+      <Marker key={topic.topic_id} position={[latValue, lonValue]}>
+        <Popup closeButton={false}>
+          <div>{addressValue}</div>
+          <div>{postalCodeValue} {regionValue}</div>
+        </Popup>
+      </Marker>
+    )
+  }
+
+  getAllMarkers() {
+    return (this.results && this.results.length > 0) ? this.results.map(this.createMarkerFromTopic) : null;
+  }
+
+  getCenter() {
+    const markers = this.getAllMarkers();
+    if (markers && markers.length > 0) {
+      markers.forEach(marker => {
+        const position = marker?.props?.position;
+        if (position && position.length == 2) {
+          this.boundaries.push([Number(position[0]), Number(position[1])]);
+        }
+      });
+      return L.latLngBounds(this.boundaries).getCenter();
+    }
+  }
+
+  configureZoom(mapInstance) {
+    mapInstance.fitBounds(L.latLngBounds(this.boundaries));
+  }
+
+  setIcon() {
+    let defaultIcon = L.icon({
+      iconUrl: icon,
+      shadowUrl: iconShadow,
+    });
+    L.Marker.prototype.options.icon = defaultIcon;
+  }
+
   renderComponent() {
+
+    this.setIcon();
+
     if (this.results !== '' && this.language !== '') {
       const searchResultList = (
         <SearchResultList
@@ -62,7 +121,26 @@ class SearchResultsWebComponent extends HTMLElement {
         ></SearchResultList>
       );
 
-      ReactDOM.render(searchResultList, this.mountPoint);
+      const center = this.getCenter();
+      const markers = this.getAllMarkers();
+      const map = (
+        center && markers &&
+          <MapContainer
+            whenCreated={mapInstance => {this.configureZoom(mapInstance)}}
+            attributionControl={false}
+            style={{height: 500}}
+            center={[center.lat, center.lng]}
+            zoom={8}
+            scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {markers}
+          </MapContainer>
+      );
+
+      ReactDOM.render(<>{map} {searchResultList}</>, this.mountPoint);
     }
   }
 }
