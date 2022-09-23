bf31d9d58070fe96909cd152f0e11ea2c53bfecc
inacfi <filipe.inacio@akros.ch>
2022-09-23 07:44:16 +0200

    removed unused packages from package.json

--- am-ui/webcomponents/create-ad-webcomponent/src/main.js
+++ am-ui/webcomponents/create-ad-webcomponent/src/main.js
@@ -1,12 +1,9 @@
 import I18nHost from './components/I18nHost.ce.vue';
 import CreateAdWebComponent from './components/CreateAd.ce.vue';
 import {defineCustomElement} from 'vue';
-import VueUploadComponent from 'vue-upload-component';
 
 
 const I18nHostElement = defineCustomElement(I18nHost);
 const CreateAdWebComponentElement = defineCustomElement(CreateAdWebComponent);
-const VueUploadComponentElement = defineCustomElement(VueUploadComponent);
 customElements.define('i18n-host', I18nHostElement);
 customElements.define('create-ad-webcomponent', CreateAdWebComponentElement);
-customElements.define('vue-upload-component', VueUploadComponentElement);
