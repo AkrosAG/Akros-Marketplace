078d3b01866a2b12cc2125d10cdff0da46af01df
Asida Yambay <86468677+asiday@users.noreply.github.com>
2022-10-21 13:32:50 +0200

    Implement sorting functionality (#183)
    Chnages:
    -Add initaial sorting by location
    -Add histry styte for the order, that is unchanged after visiting specific accommodation
    
    Close #113

--- am-ui/webcomponents/search-results-webcomponent/src/SearchResultList.jsx
+++ am-ui/webcomponents/search-results-webcomponent/src/SearchResultList.jsx
@@ -4,27 +4,58 @@ import SearchResultItem from './SearchResultItem.jsx';
 import PropTypes from 'prop-types';
 
 function SearchResultList(props) {
+
   const [results, setResults] = useState([]);
   const {t, i18n} = useTranslation();
-  useState(props.language);
 
+  useState(props.language);
   useEffect(() => {
     i18n.changeLanguage(props.language);
   }, [props.language]);
 
+  /**
+   * This Hook is used to set property result after render.
+   * Also, it executes sort method when the component is initially rendered or updated,
+   * without losing the sort type, here it is named 'order'.
+   */
   useEffect(() => {
     setResults(props.results);
+    sortResult(history.state.order, props.results);
   }, [props.results]);
 
   /**
-   * Updates a result list according to selected type of sort.
+   * Updates the result list according to selected type of sort.
+   * replaceState used to rewrite browser history, and
+   *  it changes the current history entry without adding new entries to the users' history.
    *
-   * @param {ChangeEvent} event refers to th elements property with the type of sort
+   * @param {ChangeEvent} event refers to the elements property with the type of sort
    */
   const onUpdateSortSelect = (event) => {
-    let sortedList = [...results];
+    sortResult(event.target.value, results);
+    window.history.replaceState(
+      Object.assign({}, window.history.state, {order: event.target.value}),
+      "push sort order");
+  }
 
-    switch (event.target.value) {
+  const sortTypes = [
+    {value: 'location', label: t('location')},
+    {value: 'byPriceLowToHigh', label: t('byPriceLowToHigh')},
+    {value: 'byPriceHighToLow', label: t('byPriceHighToLow')},
+    {value: 'byDateNewToOld', label: t('byDateNewToOld')},
+    {value: 'byDateOldToNew', label: t('byDateOldToNew')},
+  ];
+
+  /**
+   * Sorts a result list according to selected type of sort.
+   *
+   * @param {String} sortType refers to the selected type of sort
+   * @param {Array} resultsArray refers to the result list
+   */
+  const sortResult = (sortType, resultsArray) => {
+
+    let sortedList = [...resultsArray];
+
+    switch (sortType) {
       case 'byPriceLowToHigh' :
         sortedList.sort((a, b) =>
           (a["topic_values"].find(x => x.key === 'price')["value"] -
@@ -50,37 +81,31 @@ function SearchResultList(props) {
               a["topic_values"].find(x => x.key === 'date')["value"]) ? 1 : 0);
         break;
       case 'location' :
+      default:
         sortedList.sort((a, b) =>
           (a["topic_values"].find(x => x.key === 'region')["value"] <
             b["topic_values"].find(x => x.key === 'region')["value"]) ? -1 :
             (a["topic_values"].find(x => x.key === 'region')["value"] >
               b["topic_values"].find(x => x.key === 'region')["value"]) ? 1 : 0);
         break;
-      default:
-        sortedList = [...results];
     }
     setResults(sortedList);
   }
-
-  const sortTypes = [
-    {value: 'none', label: ''},
-    {value: 'byPriceLowToHigh', label:t('byPriceLowToHigh')},
-    {value: 'byPriceHighToLow', label:t('byPriceHighToLow')},
-    {value: 'byDateNewToOld', label:t('byDateNewToOld')},
-    {value: 'byDateOldToNew', label:t('byDateOldToNew')},
-    {value: 'location', label: t('location')},
-
-  ];
 
   return results.length === 0 ? (
     <h1 className="no-results">{t('noResults')}</h1>
   ) : (
     <div>
-      <div >
-        <label form="sortBy" >{t('sortBy')}</label>
-        <select id="sortBy" className="filter-select" onChange={onUpdateSortSelect}>
+      <div>
+        <label form="sortBy">{t('sortBy')}</label>
+        <select id="sortBy"
+                className="filter-select"
+                onChange={onUpdateSortSelect}
+                defaultValue={history.state.order}>
           {sortTypes.map((option, index) =>
-            <option data-testid="select-option" key={index} value={option.value}>
+            <option data-testid="select-option"
+                    key={index}
+                    value={option.value}>
               {option.label}
             </option>
           )}
