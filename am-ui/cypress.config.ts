5439b5d4aeb37e5b21a6fbb053bee475538625c9
FabianTschachtli <fabian@tschacht.li>
2022-11-16 15:32:29 +0100

    79: Insert ,

--- am-ui/cypress.config.ts
+++ am-ui/cypress.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   screenshotsFolder: 'cypress/screenshots',
   fixturesFolder: 'cypress/fixtures',
   e2e: {
-    baseUrl: 'http://localhost:4200'
-  }
+    baseUrl: 'http://localhost:4200',
+  },
 });
