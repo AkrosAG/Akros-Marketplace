25a7e5e25834f252fbfea8ffd1dee327b3a53725
FabianTschachtli <fabian@tschacht.li>
2022-11-17 16:30:05 +0100

    79: Refresh

--- am-ui/cypress.config.ts
+++ am-ui/cypress.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   fixturesFolder: 'cypress/fixtures',
   e2e: {
     baseUrl: 'http://localhost:4200',
-    defaultCommandTimeout: 6000,
+    defaultCommandTimeout: 30000,
   },
 });
