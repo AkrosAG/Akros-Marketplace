08b3a3f81bab195e9678a36aeb512f721f8fe5ef
FabianTschachtli <fabian@tschacht.li>
2022-11-17 16:43:58 +0100

    79: Testing

--- am-ui/cypress/e2e/spec.cy.ts
+++ am-ui/cypress/e2e/spec.cy.ts
@@ -1,6 +1,7 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/');
+    cy.contains('MarketplaceUi');
     cy.contains('Akros Marketplace');
   });
 });
