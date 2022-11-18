31a51a6b94cc6d492f7e4d2d4990d539efe9f86a
FabianTschachtli <fabian@tschacht.li>
2022-11-18 11:33:00 +0100

    79: Original Test with chromeWebSecurtity = false

--- am-ui/cypress.config.ts
+++ am-ui/cypress.config.ts
@@ -8,4 +8,5 @@ export default defineConfig({
     baseUrl: 'http://localhost:4200',
     defaultCommandTimeout: 30000,
   },
+  chromeWebSecurity: false,
 });
--- am-ui/cypress/e2e/spec.cy.ts
+++ am-ui/cypress/e2e/spec.cy.ts
@@ -1,7 +1,6 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/');
-    cy.contains('MarketplaceUi');
     cy.contains('Akros Marketplace');
   });
 });
