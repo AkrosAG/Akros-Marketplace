395d3b7bf7af1bd85fba7ecf7424c07b94de9508
Atanasov-AA <atanas.atanasov@noser-bulgaria.com>
2022-12-19 18:25:52 +0200

    Move legacy files to legacy folder

--- infrastructure/legacy/start_all_local/clean_startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker container prune -f
-docker image prune -f
-docker image rm start_all_local_marketplace-service:latest start_all_local_client-ui:latest
-docker volume rm am_db --force
-docker volume rm keycloak_db --force
-docker-compose up -d 
