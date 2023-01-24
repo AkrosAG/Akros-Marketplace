0789709ef3552fd495d353f7d04c5773d25963b9
Atanasov-AA <atanas.atanasov@noser-bulgaria.com>
2023-01-24 18:21:00 +0200

    Code refactoring after code review

--- marketplace-service/src/main/java/ch/akros/marketplace/service/controller/UserController.java
+++ marketplace-service/src/main/java/ch/akros/marketplace/service/controller/UserController.java
@@ -2,20 +2,15 @@ package ch.akros.marketplace.service.controller;
 
 import ch.akros.marketplace.api.model.UserDTO;
 import ch.akros.marketplace.api.model.UserResponseDTO;
-import ch.akros.marketplace.service.constants.BaseConstants;
+import ch.akros.marketplace.service.exceptions.UnauthorizedException;
 import ch.akros.marketplace.service.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.keycloak.KeycloakPrincipal;
-import org.keycloak.KeycloakSecurityContext;
-import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import javax.validation.Valid;
-import java.security.Principal;
 import java.util.UUID;
 
 @RestController
@@ -34,10 +29,6 @@ public class UserController {
         log.debug("UserController.deleteUser() with id " + id + " called");
 
         UUID userId = UUID.fromString(id);
-        boolean isUserAuthorized = isCurrentUserAuthorized(userId);
-        if (!isUserAuthorized) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        }
 
         userService.deleteUser(userId);
         return ResponseEntity.noContent().build();
@@ -48,41 +39,12 @@ public class UserController {
         log.debug("UserController.updateUser() with id " + id + " called");
 
         UUID userId = UUID.fromString(id);
-        boolean isUserAuthorized = isCurrentUserAuthorized(userId);
-        if (!isUserAuthorized) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        }
 
         UserResponseDTO userResponseDTO = userService.updateUser(userId, user);
         return ResponseEntity.ok(userResponseDTO);
     }
 
-    public boolean isCurrentUserAuthorized(UUID userIdToBeUpdated) {
-        KeycloakAuthenticationToken authentication = (KeycloakAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
-        Principal principal = (Principal) authentication.getPrincipal();
-
-        if (principal instanceof KeycloakPrincipal) {
-            KeycloakPrincipal<KeycloakSecurityContext> kPrincipal = (KeycloakPrincipal<KeycloakSecurityContext>) principal;
-            String userIdByToken = kPrincipal.getKeycloakSecurityContext().getToken().getSubject();
-
-            if (userIdByToken.equals(userIdToBeUpdated.toString())) {
-                log.debug("UserController.isCurrentUserAuthorized() user with id: " + userIdByToken + " updating own profile");
-                return true;
-            }
-            log.debug("UserController.isCurrentUserAuthorized() user with id: " + userIdByToken + " want to change " +
-                    "user with id: " + userIdToBeUpdated);
-        }
-
-        boolean isAdmin = authentication.getAuthorities().stream().anyMatch(grantedAuthority -> {
-            String authorityName = grantedAuthority.getAuthority();
-            return authorityName.equals(BaseConstants.USER_ROLE_ADMIN);
-        });
-        log.debug("UserController.isCurrentUserAuthorized() user with id is admin: " + isAdmin);
-
-        return isAdmin;
-    }
-
-    @ExceptionHandler({WebClientResponseException.class, IllegalArgumentException.class})
+    @ExceptionHandler({WebClientResponseException.class, IllegalArgumentException.class, UnauthorizedException.class})
     public ResponseEntity handleException(Exception ex) {
         log.debug("Exception handler: ", ex);
         if (ex instanceof WebClientResponseException) {
@@ -101,6 +63,8 @@ public class UserController {
             }
         } else if (ex instanceof IllegalArgumentException) {
             return ResponseEntity.badRequest().build();
+        } else if (ex instanceof UnauthorizedException) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
         return ResponseEntity.internalServerError().build();
     }
